4b0853c7000245e4377eb231a33b986268b0f2cb
Ivan Davletshin <j.rooft@gmail.com>
2012-10-25 22:26:52 +0400

    Added functionality to reproduce the problem

--- gen/com/example/no_network_connect/R.java
+++ gen/com/example/no_network_connect/R.java
@@ -20,7 +20,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int menu_settings=0x7f080004;
+        public static final int pass_edit=0x7f080003;
+        public static final int pass_text=0x7f080002;
+        public static final int ssid_edit=0x7f080001;
+        public static final int ssid_text=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -30,9 +34,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int title_activity_main=0x7f050003;
+        public static final int connect=0x7f050005;
+        public static final int connect_to_wpa2_wifi_network=0x7f050002;
+        public static final int menu_settings=0x7f050001;
+        public static final int passphrase=0x7f050004;
+        public static final int ssid=0x7f050003;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
--- src/com/example/no_network_connect/MainActivity.java
+++ src/com/example/no_network_connect/MainActivity.java
@@ -1,24 +1,151 @@
 package com.example.no_network_connect;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    
+    private static final String TAG = MainActivity.class.getSimpleName();
 
+    private WifiManager wm;
+    
+    private TextView ssidTv;
+    private TextView passTv;
+    
+    private BroadcastReceiver wifiStateChangeReceiver;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        
+        ssidTv = (TextView) findViewById(R.id.ssid_edit);
+        passTv = (TextView) findViewById(R.id.pass_edit);
     }
-
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        
+        wifiStateChangeReceiver = new BroadcastReceiver() {
+            
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                NetworkInfo ni = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                Log.d(TAG, "Network state changed: " + ni);
+                
+                WifiInfo wi = wm.getConnectionInfo();
+                Log.d(TAG, "Corresponding wifi info: " + wi);
+            }
+        };
+        
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        
+        registerReceiver(wifiStateChangeReceiver, filter);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        
+        if (wifiStateChangeReceiver != null) {
+            unregisterReceiver(wifiStateChangeReceiver);
+            wifiStateChangeReceiver = null;
+        }
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 
-    
+    public void onConnectClicked(final View view) {
+        view.setEnabled(false);
+        
+        // Android requires this params to be in quotes
+        final String ssid = String.format("\"%s\"", ssidTv.getText().toString());
+        final String passphrase = String.format("\"%s\"", passTv.getText().toString());
+        
+        createNetwork(ssid, passphrase);
+        connectToNetwork(ssid);
+        
+        Handler handler = new Handler();
+        
+        handler.postDelayed(new Runnable() {
+            
+            @Override
+            public void run() {
+                disableNetwork(ssid);
+            }
+            
+        }, 1000);
+
+        handler.postDelayed(new Runnable() {
+            
+            @Override
+            public void run() {
+                view.setEnabled(true);
+            }
+            
+        }, 3000);
+    }
+
+    private void createNetwork(String ssid, String passphrase) {
+        deleteNetworkIfExists(ssid);
+        
+        WifiConfiguration wc = new WifiConfiguration();
+        wc.SSID = ssid;
+        wc.preSharedKey = passphrase;
+        wm.addNetwork(wc);
+    }
+
+    private void deleteNetworkIfExists(String ssid) {
+        for (WifiConfiguration wc : wm.getConfiguredNetworks()) {
+            if (ssid.equals(wc.SSID)) {
+                wm.removeNetwork(wc.networkId);
+                wm.saveConfiguration();
+                break;
+            }
+        }
+    }
+
+    private void connectToNetwork(String ssid) {
+        for (WifiConfiguration wc : wm.getConfiguredNetworks()) {
+            if (ssid.equals(wc.SSID)) {
+                Log.d(TAG, "start connecting");
+                wm.disconnect();
+                wm.enableNetwork(wc.networkId, true);
+                wm.reconnect();
+                break;
+            }
+        }
+    }
+
+    private void disableNetwork(String ssid) {
+        for (WifiConfiguration wc : wm.getConfiguredNetworks()) {
+            if (ssid.equals(wc.SSID)) {
+                Log.d(TAG, "network disabled");
+                wm.disableNetwork(wc.networkId);
+                break;
+            }
+        }
+    }
 }
